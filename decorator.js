bc3e28af55380c7dd72fcfbd9bca981da75bbec4
Bradley Momberger <brad@bitovi.com>
2022-03-22 14:16:58 -0400

    Make decorator a CommonJS format file

--- decorator.js
+++ decorator.js
@@ -1,5 +1,8 @@
-exports.addToJQuery = function ($) {
-    var window = require('window-handle').getWindow();
+const windowHandle = require('window-handle');
+
+module.exports = {
+    addToJQuery($) {
+    var window = windowHandle.getWindow();
     var _$ = window.$;
     var _jQuery = window.jQuery;
 
@@ -20,17 +23,6 @@ exports.addToJQuery = function ($) {
 * Includes: core.js, widget.js, mouse.js, position.js, accordion.js, autocomplete.js, button.js, datepicker.js, dialog.js, draggable.js, droppable.js, effect.js, effect-blind.js, effect-bounce.js, effect-clip.js, effect-drop.js, effect-explode.js, effect-fade.js, effect-fold.js, effect-highlight.js, effect-puff.js, effect-pulsate.js, effect-scale.js, effect-shake.js, effect-size.js, effect-slide.js, effect-transfer.js, menu.js, progressbar.js, resizable.js, selectable.js, selectmenu.js, slider.js, sortable.js, spinner.js, tabs.js, tooltip.js
 * Copyright 2015 jQuery Foundation and other contributors; Licensed MIT */
 
-(function( factory ) {
-	if ( typeof define === "function" && define.amd ) {
-
-		// AMD. Register as an anonymous module.
-		define([ "jquery" ], factory );
-	} else {
-
-		// Browser globals
-		factory( jQuery );
-	}
-}(function( $ ) {
 /*!
  * jQuery UI Core 1.11.4
  * http://jqueryui.com
@@ -16628,13 +16620,9 @@ var tooltip = $.widget( "ui.tooltip", {
 		this.liveRegion.remove();
 	}
 });
-
-
-
-}));
         /* --------------------------------------------------------------------------------------------------------------------------- */
     } finally {
         window.$ = _$;
         window.jQuery = _jQuery;
     }
-};
+}};
